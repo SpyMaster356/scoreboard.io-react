0144fc0bdc23fb548d03ff25c4ba2d67050dd41f
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-01-17 22:42:39 -0500

    finish design

--- src/pages/score-history-page.view.js
+++ src/pages/score-history-page.view.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
   Paper,
   SelectField,
@@ -10,91 +10,162 @@ import {
   TableBody,
   TableRowColumn,
   IconButton,
+  Popover,
+  FlatButton,
 } from 'material-ui';
 
 import ModeEditIcon from 'material-ui/svg-icons/editor/mode-edit';
+import AddCircleOutlineIcon from 'material-ui/svg-icons/content/add-circle-outline';
+import RemoveCircleOutlineIcon from 'material-ui/svg-icons/content/remove-circle-outline';
 
 import './score-history-page.scss';
 
-const ScoreHistoryPageView = () => (
-  <div className="score-history-page">
-    <Paper className="player-select-area">
-      <SelectField
-        fullWidth={true}
-        floatingLabelText="Player"
-        value={1}
-      >
-        <MenuItem value={1} primaryText="Stephen"/>
-        <MenuItem value={2} primaryText="Stephen"/>
-        <MenuItem value={3} primaryText="Stephen"/>
-        <MenuItem value={4} primaryText="Stephen"/>
-        <MenuItem value={5} primaryText="Stephen"/>
-        <MenuItem value={6} primaryText="Stephen"/>
-        <MenuItem value={7} primaryText="Stephen"/>
-        <MenuItem value={8} primaryText="Stephen"/>
-        <MenuItem value={9} primaryText="Stephen"/>
-      </SelectField>
-    </Paper>
-    <div>
-      <Table selectable={false}>
+class ScoreHistoryPageView extends Component {
+  constructor (props) {
+    super(props);
+
+    this.onEditScore = this.onEditScore.bind(this);
+    this.onDoneEditScores = this.onDoneEditScores.bind(this);
+
+    this.state = {
+      showPopover: false,
+      anchorEl: null
+    }
+  }
+
+  onEditScore(e) {
+    console.log('ping');
+
+    this.setState({
+      showPopover: true,
+      anchorEl: e.currentTarget.parentElement
+    })
+  }
+
+  onDoneEditScores() {
+    console.log('pong');
+
+    this.setState({
+      showPopover: false
+    });
+  }
+
+  render = () => (
+    <div className="score-history-page">
+      <div className="player-select-area">
+        <SelectField
+          fullWidth={true}
+          floatingLabelText="Player"
+          value={1}
+        >
+          <MenuItem value={1} primaryText="Stephen"/>
+          <MenuItem value={2} primaryText="Stephen"/>
+          <MenuItem value={3} primaryText="Stephen"/>
+          <MenuItem value={4} primaryText="Stephen"/>
+          <MenuItem value={5} primaryText="Stephen"/>
+          <MenuItem value={6} primaryText="Stephen"/>
+          <MenuItem value={7} primaryText="Stephen"/>
+          <MenuItem value={8} primaryText="Stephen"/>
+          <MenuItem value={9} primaryText="Stephen"/>
+        </SelectField>
+      </div>
+      <Table className="scores-table" selectable={false}>
         <TableHeader
           displaySelectAll={false}
           adjustForCheckbox={false}
         >
           <TableRow>
-            <TableHeaderColumn tooltip="The round number">Round</TableHeaderColumn>
-            <TableHeaderColumn tooltip="Points scored in the round">Score</TableHeaderColumn>
-            <TableHeaderColumn tooltip="Total at end of the round">Total</TableHeaderColumn>
-            <TableHeaderColumn tooltip=""></TableHeaderColumn>
+            <TableHeaderColumn>Round</TableHeaderColumn>
+            <TableHeaderColumn>Score</TableHeaderColumn>
+            <TableHeaderColumn>Total</TableHeaderColumn>
           </TableRow>
         </TableHeader>
-        <TableBody
-          displayRowCheckbox={false}
-        >
+        <TableBody displayRowCheckbox={false}>
           <TableRow>
             <TableRowColumn>1</TableRowColumn>
-            <TableRowColumn>+4</TableRowColumn>
-            <TableRowColumn>4</TableRowColumn>
             <TableRowColumn>
-              <IconButton tooltip="Edit">
-                <ModeEditIcon />
+                <span className="score-value">
+                  -6
+                </span>
+              <IconButton
+                onClick={this.onEditScore}
+                style={{height: 48, width: 48}}
+                iconStyle={{height: 16, width: 16}}
+              >
+                <ModeEditIcon  />
               </IconButton>
             </TableRowColumn>
+            <TableRowColumn>4</TableRowColumn>
           </TableRow>
           <TableRow>
             <TableRowColumn>2</TableRowColumn>
-            <TableRowColumn>-2</TableRowColumn>
-            <TableRowColumn>2</TableRowColumn>
             <TableRowColumn>
-              <IconButton tooltip="Edit">
-                <ModeEditIcon />
+                <span className="score-value">
+                  -6
+                </span>
+              <IconButton
+                onClick={this.onEditScore}
+                style={{height: 48, width: 48}}
+                iconStyle={{height: 16, width: 16}}
+              >
+                <ModeEditIcon  />
               </IconButton>
             </TableRowColumn>
+            <TableRowColumn>2</TableRowColumn>
           </TableRow>
           <TableRow>
             <TableRowColumn>3</TableRowColumn>
-            <TableRowColumn>+6</TableRowColumn>
-            <TableRowColumn>8</TableRowColumn>
             <TableRowColumn>
-              <IconButton tooltip="Edit">
-                <ModeEditIcon />
-              </IconButton>
+                <span className="score-value">
+                  -6
+                </span>
+                <IconButton
+                  onClick={this.onEditScore}
+                  style={{height: 48, width: 48}}
+                  iconStyle={{height: 16, width: 16}}
+                >
+                  <ModeEditIcon  />
+                </IconButton>
             </TableRowColumn>
+            <TableRowColumn>8</TableRowColumn>
           </TableRow>
           <TableRow>
             <TableRowColumn>4</TableRowColumn>
-            <TableRowColumn>-3</TableRowColumn>
-            <TableRowColumn>5</TableRowColumn>
             <TableRowColumn>
-              <IconButton tooltip="Edit">
-                <ModeEditIcon />
+              <span className="score-value">
+                -6
+              </span>
+              <IconButton
+                onClick={this.onEditScore}
+                style={{height: 48, width: 48}}
+                iconStyle={{height: 16, width: 16}}
+              >
+                <ModeEditIcon  />
               </IconButton>
             </TableRowColumn>
+            <TableRowColumn>5</TableRowColumn>
           </TableRow>
         </TableBody>
       </Table>
+      <Popover
+        style={{padding: '8px'}}
+        open={this.state.showPopover}
+        anchorEl={this.state.anchorEl}
+        onRequestClose={this.onDoneEditScores}
+        anchorOrigin={{horizontal: 'middle', vertical: 'center'}}
+        targetOrigin={{horizontal: 'middle', vertical: 'center'}}
+        autoCloseWhenOffScreen={false}
+      >
+        <IconButton>
+          <RemoveCircleOutlineIcon />
+        </IconButton>
+        <span>+6</span>
+        <IconButton>
+          <AddCircleOutlineIcon />
+        </IconButton>
+      </Popover>
     </div>
-  </div>
-);
+  );
+}
 
 export default ScoreHistoryPageView;
